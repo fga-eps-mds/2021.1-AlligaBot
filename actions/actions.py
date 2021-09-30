9a63b5646995b9acdff881cfff03aabee914eb0f
douglasmonteles <silvadouglas328@gmail.com>
2021-09-29 23:53:12 -0300

    💡(#217): Criando uma funcao para definir se o download do .csv deve ser feito

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
 from os import path
 
 from typing import Any, Text, Dict, List
@@ -14,33 +15,32 @@ import pandas as pd
 import requests
 import patoolib
 
+
 class DadosDoCovidPeloCsvAction(Action):
 
     def __init__(self):
         self.diretorio = 'covid_data'
         self.arquivo = 'dados_covid_csv.gz'
         self.url = 'https://data.brasil.io/dataset/covid19/caso.csv.gz'
-        caminho_arquivo = ''
-
-        if self.arquivo_baixado_ha_mais_de_um_dia(caminho_arquivo):
-            self.resposta = requests.get(self.url)
-
-            if self.resposta.status_code == requests.codes.OK:
-                caminho_arquivo = path.join(self.diretorio, self.arquivo)
-                with open(caminho_arquivo, 'wb') as novo_arquivo:
-                    novo_arquivo.write(self.resposta.content)
-                    print(f'Download finalizado. Arquivo salvo em: {caminho_arquivo}')
-            else:
-                self.resposta.raise_for_status()
-
-            patoolib.extract_archive(caminho_arquivo, outdir=self.diretorio)
+        self.TEMPO_LIMITE_PARA_ATUALIZACAO = 1
+        self.caminho_arquivo = path.join(self.diretorio, self.arquivo)
 
     def name(self) -> Text:
         return 'action_dados_covid_baseados_em_localizacao'
     
     def arquivo_baixado_ha_mais_de_um_dia(self, caminho_do_arquivo: str):
         # os.stat(caminho_do_arquivo).
-        return True
+        SEGUNDOS_PARA_DIAS = 60 * 60 * 24
+        instante_atual = time.time()
+        instante_da_ultima_modificacao_arquivo = os.stat(caminho_do_arquivo).st_mtime
+
+        print(instante_da_ultima_modificacao_arquivo)
+
+        diferenca_de_instante_em_seg = instante_atual - instante_da_ultima_modificacao_arquivo
+        print('Diferenca de tempo em segundos', diferenca_de_instante_em_seg)
+        print('Diferenca de dias em segundos', diferenca_de_instante_em_seg / SEGUNDOS_PARA_DIAS)
+
+        return (diferenca_de_instante_em_seg / SEGUNDOS_PARA_DIAS) > self.TEMPO_LIMITE_PARA_ATUALIZACAO
 
     def run(
         self,
@@ -48,6 +48,20 @@ class DadosDoCovidPeloCsvAction(Action):
         tracker: Tracker,
         domain: Dict[Text, Any]
     ) -> List[Dict[Text, Any]]:
+
+        print('antes do if')
+        if self.arquivo_baixado_ha_mais_de_um_dia(self.caminho_arquivo):
+            print('baixando arquivo atualizado')
+            self.resposta = requests.get(self.url)
+
+            if self.resposta.status_code == requests.codes.OK:
+                with open(self.caminho_arquivo, 'wb') as novo_arquivo:
+                    novo_arquivo.write(self.resposta.content)
+                    print(f'Download finalizado. Arquivo salvo em: {self.caminho_arquivo}')
+            else:
+                self.resposta.raise_for_status()
+
+            patoolib.extract_archive(self.caminho_arquivo, outdir=self.diretorio)
 
         uf = tracker.get_slot('uf')
         cidade = tracker.get_slot('cidade')

423e77b25c24dc365a6ad1930d53bdebd17a1bc0
douglasmonteles <silvadouglas328@gmail.com>
2021-10-24 20:20:12 -0300

    🔁(#348): Refatorando as importações de actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,22 +1,7 @@
-# -*- coding: utf-8 -*-
-
-import os
-import time
-import requests
-import requests
-import patoolib
-import pandas as pd
-
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import AllSlotsReset
-from os import path
-from typing import Any, Text, Dict, List
-
-from .ResponderVacinadosEmUmEstadoAction import ResponderVacinadosEmUmEstadoAction
-from .ResponderSobreCovidAction import ResponderSobreCovidAction
-from .ResponderLembreteProximaVacinaAction import ActionCadastrarLembrete, ActionLembrarUsuario 
-from .ResponderQuantidadeInfectadosPorPeriodoAction import ResponderQuantidadeInfectadosPorPeriodoAction
-from .ResponderQuantidadeMortosPorPeriodoAction import ResponderQuantidadeMortosPorPeriodoAction
-from .ResponderQuantidadeVacinadosPorPeriodoAction import ResponderQuantidadeVacinadosPorPeriodoAction
+from actions.ResponderVacinadosEmUmEstadoAction import ResponderVacinadosEmUmEstadoAction
+from actions.ResponderSobreCovidAction import ResponderSobreCovidAction
+from actions.ResponderLembreteProximaVacinaAction import ActionCadastrarLembrete, ActionLembrarUsuario 
+from actions.ResponderQuantidadeInfectadosPorPeriodoAction import ResponderQuantidadeInfectadosPorPeriodoAction
+from actions.ResponderQuantidadeMortosPorPeriodoAction import ResponderQuantidadeMortosPorPeriodoAction
+from actions.ResponderQuantidadeVacinadosPorPeriodoAction import ResponderQuantidadeVacinadosPorPeriodoAction
 
